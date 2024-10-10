372c8e12974688a6a3c73d76814b8a847aae6bf4
nikit <nikit@MYKYTATHINKBOOK>
2024-10-10 17:49:50 +0300

    style

--- Contorio/Contorio.cs
+++ Contorio/Contorio.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Drawing;
 using Contorio.CharGraphics;
 using Contorio.CharGraphics.Widgets;
