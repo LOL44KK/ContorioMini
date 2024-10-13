f314f10ba8ff2dc547f892e9438c422611c97dd8
nikit <nikit@MYKYTATHINKBOOK>
2024-10-13 18:43:31 +0300

    ?

--- Contorio/Program.cs
+++ Contorio/Program.cs
@@ -3,8 +3,6 @@
 
     internal class Program
     {
-        Dictionary<int, Dictionary<string, int>> map = new Dictionary<int, Dictionary<string, int>>();
-
         static void Main(string[] args)
         {
             new Contorio().Run();
