5c345e3574e89c1b9006e4922c2cb566a42356f1
nikit <nikit@MYKYTATHINKBOOK>
2024-10-17 16:45:32 +0300

    upd: del music

--- Contorio/Program.cs
+++ Contorio/Program.cs
@@ -7,26 +7,6 @@ namespace Contorio
         static void Main(string[] args)
         {
             new Contorio().Run();
-            
-            /*
-            Note[] Mary =
-            {
-                new Note(Tone.B, Duration.QUARTER),
-                new Note(Tone.A, Duration.QUARTER),
-                new Note(Tone.GbelowC, Duration.QUARTER),
-                new Note(Tone.A, Duration.QUARTER),
-                new Note(Tone.B, Duration.QUARTER),
-                new Note(Tone.B, Duration.QUARTER),
-                new Note(Tone.B, Duration.HALF),
-                new Note(Tone.A, Duration.QUARTER),
-                new Note(Tone.A, Duration.QUARTER),
-                new Note(Tone.A, Duration.HALF),
-                new Note(Tone.B, Duration.QUARTER),
-                new Note(Tone.D, Duration.QUARTER),
-                new Note(Tone.D, Duration.HALF)
-            };
-            SoundPlayer.Play(Mary);
-            */
         }
     }
 }
