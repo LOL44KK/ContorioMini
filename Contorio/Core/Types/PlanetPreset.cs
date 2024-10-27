151249c644f25bd25fdee5d3ed9238ef4ea4dde5
nikit <nikit@MYKYTATHINKBOOK>
2024-10-27 22:57:57 +0200

    feat: add class PlanetPreset

---
+++ Contorio/Core/Types/PlanetPreset.cs
@@ -0,0 +1,9 @@
+﻿namespace Contorio.Core.Types
+{
+    public class PlanetPreset
+    {
+        private int _size;
+        private string _dirt;
+        private List<(string Name, int Chance)> ores;
+    }
+}
