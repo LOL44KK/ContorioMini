4423528a82af80cebad0cc9fa9267940cd143ec1
nikit <nikit@MYKYTATHINKBOOK>
2024-10-31 17:40:46 +0200

    feat: add class OrePreset

---
+++ Contorio/Core/Types/OrePreset.cs
@@ -0,0 +1,42 @@
+﻿namespace Contorio.Core.Types
+{
+    public class OrePreset
+    {
+        private string _name;
+        private double _chance;
+        private int _minClusterSize;
+        private int _maxClusterSize;
+
+        public string Name
+        {
+            get { return _name; }
+            init { _name = value; }
+        }
+
+        public double Chance
+        {
+            get { return _chance; }
+            init { _chance = value; }
+        }
+
+        public int MinClusterSize
+        {
+            get { return _minClusterSize; }
+            init { _minClusterSize = value; }
+        }
+
+        public int MaxClusterSize
+        {
+            get { return _maxClusterSize; }
+            init { _maxClusterSize = value; }
+        }
+
+        public OrePreset(string name, double chance, int minClusterSize, int maxClusterSize)
+        {
+            _name = name;
+            _chance = chance;
+            _minClusterSize = minClusterSize;
+            _maxClusterSize = maxClusterSize;
+        }
+    }
+}
