6ff3c8018cb96589e40dd0f09edb13d881eda44d
nikit <nikit@MYKYTATHINKBOOK>
2024-11-03 20:45:31 +0200

    feat: add class Сontainer

---
+++ Contorio/CharEngine/Сontainer.cs
@@ -0,0 +1,42 @@
+﻿namespace Contorio.CharEngine
+{
+    public class Сontainer
+    {
+        List<Sprite> _sprites;
+        private bool _visible;
+
+        public bool Visible
+        {
+            get { return _visible; }
+            set
+            {
+                _visible = value;
+                foreach (Sprite sprite in _sprites)
+                {
+                    sprite.Visible = _visible;
+                }
+            }
+        }
+
+        public Сontainer(List<Sprite> sprites)
+        {
+            _sprites = sprites;
+        }
+
+        public Сontainer()
+        {
+            _sprites = new List<Sprite>();
+        }
+
+        public void AddSprite(Sprite sprite)
+        {
+            _sprites.Add(sprite);
+            sprite.Visible = _visible;
+        }
+
+        public void RemoveSprite(Sprite sprite)
+        {
+            _sprites.Remove(sprite);
+        }
+    }
+}
