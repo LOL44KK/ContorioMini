3ae237d5e9135c011d499825b6fac8cd5b304f38
nikit <nikit@MYKYTATHINKBOOK>
2024-11-05 20:47:41 +0200

    feat: add Container and TabContainer

---
+++ Contorio/CharEngine/Container.cs
@@ -0,0 +1,39 @@
+﻿namespace Contorio.CharEngine
+{
+    public class Container
+    {
+        private List<Sprite> _sprites;
+        private bool _visible;
+
+        public bool Visible
+        {
+            get { return _visible; }
+            set
+            {
+                _visible = value;
+                foreach (Sprite sprite in _sprites)
+                {
+                    sprite.Visible = _visible;
+                }
+            }
+        }
+
+        public Container(List<Sprite> sprites, bool visible)
+        {
+            _sprites = sprites;
+            _visible = visible;
+        }
+
+        public Container(bool visible = true) : this(new List<Sprite>(), visible) { }
+
+        public void AddSprite(Sprite sprite)
+        {
+            _sprites.Add(sprite);
+        }
+
+        public void RemoveSprite(Sprite sprite)
+        {
+            _sprites.Remove(sprite);
+        }
+    }
+}
---
+++ Contorio/CharEngine/TabContainer.cs
@@ -0,0 +1,53 @@
+﻿namespace Contorio.CharEngine
+{
+    public class TabContainer
+    {
+        private List<Container> _containers;
+        private int _selectedTab;
+
+        public int SelectedTab
+        {
+            get { return _selectedTab; }
+            set 
+            { 
+                _selectedTab = value;
+                foreach (var container in _containers)
+                {
+                    container.Visible = false;
+                }
+                _containers[_selectedTab].Visible = true;
+            }
+        }
+
+        public TabContainer()
+        {
+            _containers = new List<Container>();
+        }
+
+        public void AddSprite(int indexTab, Sprite sprite)
+        {
+            _containers[indexTab].AddSprite(sprite);
+        }
+
+        public void RemoveSprite(int indexTab, Sprite sprite)
+        {
+            _containers[indexTab].RemoveSprite(sprite);
+        }
+
+        public void NextTab()
+        {
+            if (_selectedTab < _containers.Count - 1)
+            {
+                SelectedTab = _selectedTab + 1;
+            }
+        }
+
+        public void PreviousTab()
+        {
+            if (_selectedTab > 0)
+            {
+                SelectedTab = _selectedTab - 1;
+            }
+        }
+    }
+}
